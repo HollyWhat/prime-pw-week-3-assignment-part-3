8627cf56b3ab06d29503b5ead466343e108d408b
Holly Drazenovich <hollydraze14@gmail.com>
2022-06-18 18:03:02 -0500

    question 5 completed

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -11,15 +11,27 @@ let partsNeeded = 40;
 //    the following numbers: 3, 5, -6, 0, 7, 11
 console.log('2. Array of supplyChanges:');
 
+let supplyChanges = [3, 5, -6, 0, 7, 11];
+
+
 // 3. Console log the value of the second item in the 'supplyChanges' array
 console.log('3. Second supplyChange is:');
+
+console.log("second supply change is",  supplyChanges[1] );
+
 
 // 4. The last item was added by mistake. Remove it from the 'supplyChanges' 
 //    array & console.log the value removed.
 console.log('4. Removed item:');
 
+
+console.log("last item removed", supplyChanges.pop() );//should be 11 
+
+
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Adding 25 to supplyChanges.');
+console.log("added 25 to the list", supplyChanges.push(25) );
+console.log("checking the array", supplyChanges );
 
 // 6. Write a `for` loop that shows each value in the 'supplyChanges' array
 //    Use a console.log formatted as follows, where x is the value from the array
