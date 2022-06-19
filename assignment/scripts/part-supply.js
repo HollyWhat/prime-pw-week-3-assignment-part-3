396ffeccb48d10160a2d9c0adc724a26b22e76a4
Holly Drazenovich <hollydraze14@gmail.com>
2022-06-19 15:08:57 -0500

    contiune working on question 6

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -41,8 +41,9 @@ console.log("checking the array", supplyChanges );
 console.log('6. Showing supplyChanges...');
 
 
-for(){
-    
+for(let i = 0; i >= supplyChanges.length; i++){
+    console.log('here are my supply changes', supplyChanges[i] );
+
 }
 
 
